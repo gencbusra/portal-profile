311392167629b20719d6fb337e6cbea11bb2b01e
gencbusra <43874630+gencbusra@users.noreply.github.com>
2018-12-28 22:42:25 +0300

    Add files via upload

---
+++ views/backend/default/view.php
@@ -0,0 +1,55 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model vendor\kouosl\profil\models\Profile */
+
+$this->title = $model->name;
+$this->params['breadcrumbs'][] = ['label' => 'Profiles', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+\yii\web\YiiAsset::register($this);
+?>
+<div class="profile-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'user_id',
+            'name',
+            'surname',
+            'birthday',
+            'school',
+            'start_date',
+            'graduation_year',
+            'job',
+            'company',
+            'department',
+            'phone_number',
+            'mail_address:ntext',
+            'abilities:ntext',
+            'experiences:ntext',
+            'hobbies:ntext',
+            'link:ntext',
+            'status',
+            'photo',
+            'image_url:url',
+        ],
+    ]) ?>
+
+</div>
