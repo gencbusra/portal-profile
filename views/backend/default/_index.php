228aad4256eafdc9b2acbf52b96f3dbedc9009ad
gencbusra <43874630+gencbusra@users.noreply.github.com>
2019-01-04 02:14:24 +0300

    Update _index.php

--- views/backend/default/_index.php
+++ views/backend/default/_index.php
@@ -1,18 +1,51 @@
 <?php
-use kouosl\theme\helpers\Html;
-use kouosl\theme\widgets\Portlet;
 
-$this->title = 'Index Sample';
-$data['title'] = Html::encode($this->title);
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel vendor\kouosl\profil\models\ProfileSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Profiles';
 $this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="profile-index">
 
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
+    <p>
+        <?= Html::a('Create Profile', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
 
-Portlet::begin(['title' => $this->title,'subTitle' => 'samples data','icon' => 'glyphicon glyphicon-cog']);
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
 
-echo $this->render('index');
+            'id',
+            'name',
+            'surname',
+            'birthday',
+            //'school',
+            //'start_date',
+            //'graduation_year',
+            //'job',
+            //'company',
+            //'department',
+            //'phone_number',
+            //'mail_address:ntext',
+            //'abilities:ntext',
+            //'experiences:ntext',
+            //'hobbies:ntext',
+            //'link:ntext',
+            //'status',
+            //'photo',
+            //'image_url:url',
 
-Portlet::end();
-
-
-
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
