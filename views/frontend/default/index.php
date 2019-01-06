2d9c5601887068889879c1dca8243d776de4e6f4
gencbusra <43874630+gencbusra@users.noreply.github.com>
2019-01-06 15:00:09 +0300

    Add files via upload

---
+++ views/frontend/default/index.php
@@ -0,0 +1,16 @@
+<style type="text/css">
+    .btn-purple{background-color:#8A2BE2;color:#FFF;}
+    .btn-black:hover{color:#FFF;};
+</style>
+<?= $this->title = 'Modül çalışıyor.'; ?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Kou Osl Yii2 App</h1>
+
+        <p class="lead">Örnek uygulamayı başarılı bir şekilde çalıştırdınız.</p>
+
+        <p><a class="btn btn-lg btn-purple" href="#">Modüller ve konfürgasyon!</a></p>
+    </div>
+
+</div>
