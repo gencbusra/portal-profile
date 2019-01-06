1c1ce45de12965d824238ba1ef53dbd5dc80160d
gencbusra <43874630+gencbusra@users.noreply.github.com>
2019-01-06 13:28:37 +0300

    Update create.php

--- views/backend/default/create.php
+++ views/backend/default/create.php
@@ -9,12 +9,18 @@ $this->title = 'Create Profile';
 $this->params['breadcrumbs'][] = ['label' => 'Profiles', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<style type="text/css">
+    .btn-purple{background-color:#8A2BE2;color:#FFF;}
+    .btn-black:hover{color:#FFF;};
+</style>
 <div class="profile-create">
 
     <h1><?= Html::encode($this->title) ?></h1>
+    
 
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
+
 
 </div>
