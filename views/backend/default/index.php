2d3b345cd72f23a97c64409d786095e65c227388
gencbusra <43874630+gencbusra@users.noreply.github.com>
2019-01-06 15:03:50 +0300

    Add files via upload

---
+++ views/backend/default/index.php
@@ -0,0 +1,15 @@
+<?= $this->title = 'Profile Admin'; ?>
+<style type="text/css">
+    .btn-purple{background-color:#8A2BE2;color:#FFF;}
+    .btn-black:hover{color:#FFF;};
+</style>
+<div class="site-index">
+
+    <div class="jumbotron">
+
+        
+        <p><a class="btn btn-lg btn-purple" href="http://portal.kouosl/profile/profile/index">Tüm Profiller</a></p>
+        
+    </div>
+
+</div>
